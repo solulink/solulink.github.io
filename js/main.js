92cc37f5272621e47faa92504a9585b6632dc1ce
Antone Trinh <solulink@gmail.com>
2021-08-17 18:46:52 -0400

    update to cached site

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../sw_cached_pages.js')
+      .register('../sw_cached_sites.js')
       .then(reg => console.log('Service Worker: Registered (Pages)'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
